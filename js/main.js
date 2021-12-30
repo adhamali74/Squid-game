8a25bc61e742cde2f84eab6e95abe56156b4b2d3
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2021-12-30 05:00:42 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
+ 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
